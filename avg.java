bc80e3b2f92c11f0efe915f46a52cb0444ed62e7
Prajwal Gowda H G <169121568+Prajwalgowda8660@users.noreply.github.com>
2024-06-04 18:59:45 +0530

    Create avg.java

---
+++ avg.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner read = new Scanner(System.in);
+        int t = read.nextInt();
+        for (int i = 0; i < t; i++) {
+            int A = read.nextInt();
+            int C = read.nextInt();
+            if (A % 2 == 0 && C % 2 == 0) {
+                int B = (A + C) / 2;
+                System.out.println(B);
+            }
+            else if (A % 2 != 0 && C % 2 != 0) {
+                int B = (A + C) / 2;
+                System.out.println(B);
+            }
+            else {
+                System.out.println(-1);
+            }
+        }
+    }
+}
